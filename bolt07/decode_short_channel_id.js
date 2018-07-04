75bc0a31877f25f4d064c10bdd76baec11f430e3
Alex Bosworth <alex.bosworth@gmail.com>
2018-07-04 14:18:35 -0700

    Add bolt 07 short channel id parsing and encoding

---
+++ bolt07/decode_short_channel_id.js
@@ -0,0 +1,43 @@
+const BN = require('bn.js');
+
+const blockIndexByteLength = 4;
+const decimalBase = 10;
+const heightByteLength = 4;
+const outputIndexByteLength = 2;
+
+/** Decode a short channel id into components
+
+  {
+    id: <BOLT 7 Encoded Short Channel Id String>
+  }
+
+  @returns
+  {
+    block_height: <Channel Funding Transaction Inclusion Block Height Number>
+    block_index: <Channel Funding Transaction Inclusion Block Position Number>
+    output_index: <Channel Funding Transaction Output Index Number>
+  }
+*/
+module.exports = args => {
+  const id = new BN(args.id, decimalBase).toBuffer();
+
+  const height = Buffer.alloc(heightByteLength);
+  const index = Buffer.alloc(blockIndexByteLength);
+  const vout = Buffer.alloc(outputIndexByteLength);
+
+  // Skip the first byte of height, which will be zero due to BE 3byte encoding
+  id.copy(height, 1);
+
+  // Skip the first byte of index, also zero. Write into id after the 3rd byte
+  id.copy(index, 1, 3);
+
+  // Write the vout into the final 2 bytes
+  id.copy(vout, 0, 6);
+
+  return {
+    block_height: height.readUInt32BE(0),
+    block_index: index.readUInt32BE(0),
+    output_index: vout.readUInt16BE(0),
+  };
+};
+
---
+++ bolt07/index.js
@@ -0,0 +1,5 @@
+const decodeShortChannelId = require('./decode_short_channel_id');
+const encodeShortChannelId = require('./encode_short_channel_id');
+
+module.exports = {decodeShortChannelId, encodeShortChannelId};
+
