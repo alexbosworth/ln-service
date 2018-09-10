ecc347fee80fadb92883d5b94a17b353094be011
Alex Bosworth <alex.bosworth@gmail.com>
2018-09-09 20:44:36 -0700

    omit unknown elements in closed channels listing

--- lightning/get_closed_channels.js
+++ lightning/get_closed_channels.js
@@ -1,4 +1,5 @@
 const decBase = 10;
+const emptyTxId = '0000000000000000000000000000000000000000000000000000000000000000';
 const outpointSeparator = ':';
 
 /** Get closed out channels
@@ -18,19 +19,19 @@ const outpointSeparator = ':';
   {
     channels: [{
       capacity: <Closed Channel Capacity Tokens Number>
-      close_confirm_height: <Channel Close Confirmation Height Number>
-      close_transaction_id: <Closing Transaction Id Hex String>
+      [close_confirm_height]: <Channel Close Confirmation Height Number>
+      [close_transaction_id]: <Closing Transaction Id Hex String>
       final_local_balance: <Channel Close Final Local Balance Tokens Number>
       final_time_locked_balance: <Closed Channel Timelocked Tokens Number>
-      id: <Closed Channel Id String>
-      is_breach_close: n.close_type === 'BREACH_CLOSE',
-      is_cooperative_close: n.close_type === 'COOPERATIVE_CLOSE',
-      is_funding_cancel: n.close_type === 'FUNDING_CANCELED',
-      is_local_force_close: n.close_type === 'LOCAL_FORCE_CLOSE',
-      is_remote_force_close: n.close_type === 'REMOTE_FORCE_CLOSE',
-      partner_public_key: n.remote_pubkey,
-      transaction_id: txId,
-      transaction_vout: parseInt(vout, decBase),
+      [id]: <Closed Channel Id String>
+      is_breach_close: <Is Breach Close Bool>
+      is_cooperative_close: <Is Cooperative Close Bool>
+      is_funding_cancel: <Is Funding Cancelled Close Bool>
+      is_local_force_close: <Is Local Force Close Bool>
+      is_remote_force_close: <Is Remote Force close Bool>
+      partner_public_key: <Partner Public Key Hex String>
+      transaction_id: <Channel Funding Transaction Id Hex String>
+      transaction_vout: <Channel Funding Output Index Number>
     }]
   }
 */
@@ -69,7 +70,7 @@ module.exports = (args, cbk) => {
           throw new Error('ExpectedCloseChannelOutpoint');
         }
 
-        if (!n.close_height) {
+        if (n.close_height === undefined) {
           throw new Error('ExpectedChannelCloseHeight');
         }
 
@@ -91,13 +92,15 @@ module.exports = (args, cbk) => {
 
         const [txId, vout] = n.channel_point.split(outpointSeparator);
 
+        const hasCloseTx = n.closing_tx_hash !== emptyTxId;
+
         return {
           capacity: parseInt(n.capacity, decBase),
-          close_confirm_height: n.close_height,
-          close_transaction_id: n.closing_tx_hash,
+          close_confirm_height: !!n.close_height ? n.close_height : undefined,
+          close_transaction_id: hasCloseTx ? n.closing_tx_hash : undefined,
           final_local_balance: parseInt(n.settled_balance, decBase),
           final_time_locked_balance: parseInt(n.time_locked_balance, decBase),
-          id: n.chan_id,
+          id: n.chan_id !== '0' ? n.chan_id : undefined,
           is_breach_close: n.close_type === 'BREACH_CLOSE',
           is_cooperative_close: n.close_type === 'COOPERATIVE_CLOSE',
           is_funding_cancel: n.close_type === 'FUNDING_CANCELED',
