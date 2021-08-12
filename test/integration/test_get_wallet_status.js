b8dc097c0647df744c16efd870e079117fad7b8d
Alex Bosworth <alex.bosworth@gmail.com>
2021-08-12 09:54:11 -0700

    add test for get wallet status

---
+++ test/integration/test_get_wallet_status.js
@@ -0,0 +1,45 @@
+const {test} = require('@alexbosworth/tap');
+
+const {getWalletStatus} = require('./../../');
+const {getWalletVersion} = require('./../../');
+const {spawnLnd} = require('./../macros');
+const {unauthenticatedLndGrpc} = require('./../../');
+const {waitForTermination} = require('./../macros');
+
+// Getting the wallet status should return info about its state
+test(`Get wallet status`, async ({end, equal, strictSame}) => {
+  const spawned = await spawnLnd({});
+
+  const pubKey = spawned.public_key;
+
+  const {lnd} = unauthenticatedLndGrpc({
+    cert: spawned.lnd_cert,
+    socket: `localhost:${spawned.rpc_port}`,
+  });
+
+  const {version} = await getWalletVersion({lnd: spawned.lnd});
+
+  try {
+    const result = await getWalletStatus({lnd});
+
+    strictSame(result, {is_active: true}, 'Got expected state');
+  } catch (err) {
+    switch (version) {
+    case '0.11.0-beta':
+    case '0.11.1-beta':
+    case '0.12.0-beta':
+    case '0.12.1-beta':
+      strictSame(err, [501, 'GetWalletStatusMethodUnsupported']);
+      break;
+
+    default:
+      strictSame(err, null, 'No error is expected');
+    }
+  }
+
+  spawned.kill({});
+
+  await waitForTermination({lnd: spawned.lnd});
+
+  return end();
+});
